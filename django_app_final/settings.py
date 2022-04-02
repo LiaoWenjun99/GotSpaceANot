20f80fed7ec203c88aae3e60d6cfad1162e526ec
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-03 07:25:48 +0800

    Update settings.py

--- django_app_final/settings.py
+++ django_app_final/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'django_app_final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
